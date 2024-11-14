4b01412a3984a108b7fa2f4961fd85271f58984d
Lucas-Fernandes02 <alfds10@hotmail.com>
2024-11-13 23:40:04 -0300

    Add feature: car/product registration page functionality

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,12 @@
+import { Container } from "../../components/container"
+import { DashboardHeader } from "../../components/panelheader"
+
 export function Dashboard(){
     return(
-        <div>
-            <h1>Página Dashboard</h1>
-        </div>
+        <Container>
+            <DashboardHeader>
+                
+            </DashboardHeader>
+        </Container>
     )
 }
