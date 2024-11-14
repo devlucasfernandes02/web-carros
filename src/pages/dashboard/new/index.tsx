83a1e38c7d8b87e17c6f481de3c4c602236d24ae
Lucas-Fernandes02 <alfds10@hotmail.com>
2024-11-14 01:14:08 -0300

    feat: configure image upload and integrate with database

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,11 +1,22 @@
+import { ChangeEvent, useState, useContext} from "react";
 import { Container } from "../../../components/container";
 import { DashboardHeader } from "../../../components/panelheader";
-import { FiUpload } from "react-icons/fi";
+import { FiUpload, FiTrash } from "react-icons/fi";
 
 import { useForm } from "react-hook-form";
 import { Input } from "../../../components/input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { AuthContext } from "../../../contexts/AuthContext";
+import {v4 as uuidV4} from 'uuid'
+
+import { storage } from "../../../services/firebaseConnection";
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL,
+    deleteObject
+} from 'firebase/storage'
 
 const schema = z.object({
     name: z.string().min(1, "O campo nome é obrigatório"),
@@ -22,14 +33,75 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
+interface ImageItemProps{
+    uid: string;
+    name: string;
+    previewURL: string;
+    url: string;
+}
+
 export function New(){
+    const { user } = useContext(AuthContext)
     const { register, handleSubmit, formState: { errors }, reset} = useForm<FormData>({
         resolver: zodResolver(schema),
         mode: "onChange"
     })
 
+    const [carImages, setCarImages] = useState<ImageItemProps[]>([])
+
+    async function handleFile(e: ChangeEvent<HTMLInputElement>){
+        if(e.target.files && e.target.files[0]){
+            const image = e.target.files[0]
+
+            if(image.type === 'image/jpeg' || image.type === 'image/png'){
+               await handleUpload(image)
+            } else {
+                alert("Envie uma imagem jpeg ou png!")
+
+            }
+        }
+    }
+
+    async function handleUpload(image: File){
+        if(!user?.uid){
+            return;
+        }
+
+        const currentUid = user?.uid;
+        const uidImage = uuidV4();
+
+        const uploadRef = ref(storage, `images/${currentUid}/${uidImage}`)
+
+        uploadBytes(uploadRef, image)
+        .then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((downloadURL) => {
+                const imageItem = {
+                    name: uidImage,
+                    uid: currentUid,
+                    previewURL: URL.createObjectURL(image),
+                    url: downloadURL,
+                }
+
+                setCarImages((images) => [...images, imageItem] )
+            })
+        })
+    }
+
     function onSubmit(data: FormData){
         console.log(data);
+    }
+
+     async function handleDeleteImage(item: ImageItemProps){
+        const imagePath = `images/${item.uid}/${item.name}`;
+
+        const imageRef = ref(storage, imagePath);
+
+        try {
+            await deleteObject(imageRef);
+            setCarImages(carImages.filter((car) => car.url !== item.url))
+        }catch(err){
+            console.log("ERRO AO DELETAR")
+        }
     }
 
     return(
@@ -42,9 +114,27 @@ export function New(){
                         <FiUpload size={30} color="#000"/>
                     </div>
                     <div className="cursor-pointer">
-                        <input type="file" accept="image/*" className="opacity-0 cursor-pointer" />
+                        <input 
+                            type="file" 
+                            accept="image/*" 
+                            className="opacity-0 cursor-pointer" 
+                            onChange={handleFile} 
+                        />
                     </div>
                 </button>
+
+                {carImages.map(item => (
+                    <div key={item.name} className="w-ful h-32 flex items-center justify-center relative" onClick={() => handleDeleteImage(item)}>
+                        <button className="absolute">
+                            <FiTrash size={28} color="#FFF"/>
+                        </button>
+                        <img 
+                            src={item.previewURL}
+                            className="rounded-lg w-full h-32 object-cover"
+                            alt="Foto do carro"
+                        />
+                    </div>
+                ))}
             </div>
 
             <div className="w-full bg-white rounded-lg p-3 flex flex-col sm:flex-row items-center gap-2 mt-2">
